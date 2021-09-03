a369779a5749c641f18996dc52e273f20910d91c
Stefan Rachkov <astrex17@abv.bg>
2021-09-03 12:23:01 +0300

    Added the 5th Homework

---
+++ Homework - 5. Healthbar - UI/Assets/Scripts/Health.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using UnityEngine;
+using static UnityEngine.Mathf;
+using Random = UnityEngine.Random;
+
+public class Health : MonoBehaviour {
+
+	[SerializeField]
+	private int maxHealth = 100;
+	[SerializeField]
+	private int health = 100;
+
+	public HealthBar healthBar;
+
+	private Animator animator;
+	public GameObject cross;
+	
+	// Better name for this boolean should be
+	// immune, but for now we stay with dodge
+	public bool bDodge = false;
+
+	void Start() {
+		animator = GetComponent<Animator>();
+		animator.SetInteger("Health", maxHealth);
+		health = maxHealth;
+		
+		healthBar.SetMaxHealth(maxHealth);
+	}
+
+	public int GetMaxHP()
+	{
+		return this.maxHealth;
+	}
+
+	public int GetCurrentHP()
+	{
+		return this.health;
+	}
+
+	public void SpawnCross() {
+		Vector2 spawnPosition = new Vector2 {
+			x = transform.position.x,
+			y = -0.1f
+		};
+		Instantiate(cross, spawnPosition, Quaternion.identity);
+	}
+
+	public void Die() {
+		Destroy(gameObject);
+	}
+
+	public void TakeDamage() {
+		if (bDodge)
+		{
+			// We log which character is dodging at the moment
+			Debug.Log(animator.tag + " Dodged");
+			// and then we terminate TakeDamage() function
+			return;
+		}
+		
+		int damage = 10;
+		health = Max(health - damage, 0);
+		animator.SetInteger("Health", health);
+		animator.SetTrigger("TookDamage");
+		
+		healthBar.SetHealth(health - damage);
+		Debug.Log("Took Damage");
+		
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision) {
+		if (collision.transform.parent != transform
+			&& collision.gameObject.CompareTag("Hitbox")) 
+		{
+			TakeDamage();
+		}
+	}
+}
